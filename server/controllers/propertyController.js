0232d2755a832b04e7df82e03ee50346c25b8d88
imrankhan919 <imrankhan919.ik@gmail.com>
2025-02-26 18:01:46 +0530

    property_added

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -1,3 +1,6 @@
+const expressAsyncHandler = require("express-async-handler");
+const Property = require("../models/propertySchema");
+
 const getProperties = async (req, res) => {
   res.send("All Properties");
 };
@@ -6,9 +9,16 @@ const getProperty = async (req, res) => {
   res.send("Single Property");
 };
 
-const addProperty = async (req, res) => {
-  res.send("Property Added");
-};
+const addProperty = expressAsyncHandler(async (req, res) => {
+  const newProperty = await Property.create(req.body);
+
+  if (!newProperty) {
+    res.status(400);
+    throw new Error("Property Not Created");
+  }
+
+  res.status(201).json(newProperty);
+});
 
 const updateProperty = async (req, res) => {
   res.send("Property Updated!!");
